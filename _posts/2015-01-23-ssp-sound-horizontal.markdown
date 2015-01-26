7df046018ad746de236816f3bbb91209e13465d3
Federico Castaneda <castanedafederico@gmail.com>
2015-01-25 21:57:20 -0500

    updating site

--- _posts/2015-01-23-ssp-sound-horizontal.markdown
+++ _posts/2015-01-23-ssp-sound-horizontal.markdown
@@ -6,9 +6,20 @@ date:       2015-01-23 12:00:00
 author:     "federico"
 ---
 
-<p>horizontal buttons test</p>
+<p>horizontal buttons test</p>  
+<p>width: 730px;</p>  
+
 
 <div class="sound-feature">
-    <button id="b00" class="b00">Play "car"</button>
-    <button id="b01" class="b01">Play "laughing"</button>
+
+<ul id="horizontal-s" class="sound-button-list">
+	<li><a id="b00" class="sound-button" href="#">Rustling Leaves</a></li>
+	<li><a id="b01" class="sound-button" href="#">Stream Flowing</a></li>
+	<li><a id="b02" class="sound-button" href="#">Normal Conversation</a></li>
+	<li><a id="b03" class="sound-button" href="#">Laughter</a></li>
+	<li><a id="b04" class="sound-button" href="#">Car</a></li>
+	<li><a id="b05" class="sound-button" href="#">Truck</a></li>
+	<li><a id="b06" class="sound-button" href="#">Jet</a></li>
+</ul>
+    
 </div>
--- _site/js/ssp-sound.js
+++ _site/js/ssp-sound.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-        var audiosources = ["Rustling Leaves", "Stream flowing", "Normal Conversation", "Laughter", "Car", "Truck", "Jet"];
+        var audiosources = ["Rustling Leaves", "Stream Flowing", "Normal Conversation", "Laughter", "Car", "Truck", "Jet"];
 
         $("#audio-labels-slider")
             .slider({ min: 0, max: audiosources.length-1, value: 3 })
@@ -30,24 +30,31 @@ $(document).ready(function(){
 
 
         $(".ui-slider-pip-0").on("click", function(){
+	        ion.sound.stop();
             ion.sound.play("leaves");
         });
         $(".ui-slider-pip-1").on("click", function(){
+	        ion.sound.stop();
             ion.sound.play("stream");
         });
         $(".ui-slider-pip-2").on("click", function(){
+	        ion.sound.stop();
             ion.sound.play("conversation");
         });
         $(".ui-slider-pip-3").on("click", function(){
+	        ion.sound.stop();
             ion.sound.play("laughing");
         });        
         $(".ui-slider-pip-4").on("click", function(){
+	        ion.sound.stop();
             ion.sound.play("car");
         });
         $(".ui-slider-pip-5").on("click", function(){
+	        ion.sound.stop();
             ion.sound.play("truck");
         });
         $(".ui-slider-pip-6").on("click", function(){
+	        ion.sound.stop();
             ion.sound.play("jet");
         });
         
@@ -55,26 +62,39 @@ $(document).ready(function(){
         
         
         
-        $("#b00").on("click", function(){
+        $("#b00").on("click", function(e){
+	        ion.sound.stop();
             ion.sound.play("leaves");
+            e.preventDefault();
         });
-        $("#b01").on("click", function(){
+        $("#b01").on("click", function(e){
+	        ion.sound.stop();
             ion.sound.play("stream");
+            e.preventDefault();
         });        
-        $("#b02").on("click", function(){
+        $("#b02").on("click", function(e){
+	        ion.sound.stop();
             ion.sound.play("conversation");
+            e.preventDefault();
         });        
-        $("#b03").on("click", function(){
+        $("#b03").on("click", function(e){
+	        ion.sound.stop();
             ion.sound.play("laughing");
+            e.preventDefault();
         });
-        $("#b04").on("click", function(){
+        $("#b04").on("click", function(e){
+	        ion.sound.stop();
             ion.sound.play("car");
         });  
-        $("#b05").on("click", function(){
+        $("#b05").on("click", function(e){
+	        ion.sound.stop();
             ion.sound.play("truck");
+            e.preventDefault();
         });
-        $("#b06").on("click", function(){
+        $("#b06").on("click", function(e){
+	        ion.sound.stop();
             ion.sound.play("jet");
+            e.preventDefault();
         });  
 
 
